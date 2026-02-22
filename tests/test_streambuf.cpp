25219874f7d983a378be140cf009c3de4ff28aa8
yongminari <easyid21c@gmail.com>
2026-02-22 23:30:35 +0900

    modify environment and test code

--- tests/test_streambuf.cpp
+++ tests/test_streambuf.cpp
@@ -1,34 +1,52 @@
 #include <gtest/gtest.h>
-#include <streambuf>
-#include <vector>
+#include <boost/asio/streambuf.hpp>
+#include <iostream>
 #include <string>
+#include <ostream>
+#include <istream>
 
-// Simple custom streambuf for testing purposes
-class SimpleStreambuf : public std::streambuf {
-public:
-    SimpleStreambuf(std::vector<char>& buffer) {
-        setg(buffer.data(), buffer.data(), buffer.data() + buffer.size());
-    }
+TEST(AsioStreambufTest, BasicWriteAndRead) {
+    boost::asio::streambuf sbuf;
+    
+    // 1. 버퍼에 데이터 쓰기 (std::ostream 사용)
+    std::ostream os(&sbuf);
+    os << "hello boost asio";
+    
+    // 2. 버퍼 크기 확인
+    EXPECT_EQ(sbuf.size(), 16);
+    
+    // 3. 버퍼에서 데이터 읽기 (std::istream 사용)
+    std::istream is(&sbuf);
+    std::string word1, word2, word3;
+    is >> word1 >> word2 >> word3;
+    
+    EXPECT_EQ(word1, "hello");
+    EXPECT_EQ(word2, "boost");
+    EXPECT_EQ(word3, "asio");
+    
+    // 4. 읽은 후 버퍼가 비었는지 확인
+    EXPECT_EQ(sbuf.size(), 0);
+}
 
-protected:
-    // Basic underflow implementation for demonstration
-    int_type underflow() override {
-        if (gptr() < egptr()) {
-            return traits_type::to_int_type(*gptr());
-        }
-        return traits_type::eof();
-    }
-};
-
-TEST(StreambufTest, BasicRead) {
-    std::string data = "hello world";
-    std::vector<char> buffer(data.begin(), data.end());
-    SimpleStreambuf sbuf(buffer);
-    std::istream in(&sbuf);
-
+TEST(AsioStreambufTest, PrepareAndCommit) {
+    boost::asio::streambuf sbuf;
+    
+    // 1. 5바이트 공간 확보
+    auto bufs = sbuf.prepare(5);
+    
+    // 2. 확보된 공간에 직접 데이터 쓰기
+    std::string data = "hello";
+    boost::asio::buffer_copy(bufs, boost::asio::buffer(data));
+    
+    // 3. 데이터가 들어갔음을 알림 (commit)
+    sbuf.commit(5);
+    
+    EXPECT_EQ(sbuf.size(), 5);
+    
+    // 4. 내용 확인
     std::string result;
-    in >> result;
-
+    std::istream is(&sbuf);
+    is >> result;
     EXPECT_EQ(result, "hello");
 }
 
